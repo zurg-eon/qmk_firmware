2255dd407e0155bdf3594f7ed5a976838de7a9e3
Zurg Eon <bjornar.isaksen@gmail.com>
2020-01-31 11:07:14 +0100

    Custom rules.mk to my keymap

--- keyboards/primekb/prime_m/rules.mk
+++ keyboards/primekb/prime_m/rules.mk
@@ -30,11 +30,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-<<<<<<< HEAD
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
 TAP_DANCE_ENABLE = yes
-=======
-HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
->>>>>>> 31e4583f4a171823cfa639cc6a9795e892c0455f
 
 LAYOUTS = ortho_5x6 numpad_5x6
