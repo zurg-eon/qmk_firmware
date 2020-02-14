18fc47584382f797373e85fefd437074241b65a1
Zurg Eon <bjornar.isaksen@gmail.com>
2020-02-14 23:54:38 +0100

    Reverted change to rules.mk

--- keyboards/primekb/prime_m/rules.mk
+++ keyboards/primekb/prime_m/rules.mk
@@ -31,6 +31,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs (+400)
-TAP_DANCE_ENABLE = yes
+
+# test 
 
 LAYOUTS = ortho_5x6 numpad_5x6
